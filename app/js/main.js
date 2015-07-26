a2829ef912f5187315831317b75cb43c2353cfaa
Anup Arakkal <anup.anup2001@gmail.com>
2015-07-26 22:01:01 +0530

    Added autocomplete input box

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,12 @@
 (function(){
     var app = angular.module("main", []);
+    app.controller("formController", function(){
+        this.temp = "Hello World";
+        $( "#ipFundName" ).autocomplete({
+          source: [ "c++", "java", "php", "coldfusion", "javascript", "asp", "ruby" ]
+        });
+        this.onSubmit = function(){
+            alert("Hello World");
+        };
+    });
 })();
